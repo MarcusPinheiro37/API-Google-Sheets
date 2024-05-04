254440b82b770946306e15e17026df48f37efc86
Marcus Pinheiro <marcusmagno34@gmail.com>
2024-05-04 00:08:41 -0300

    Centralizado os codigos da planilha de gastos

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import carregarDadosAssinatura from './planilhas/Finanças/assinatura.js';
+import carregarDadosCartao from './planilhas/Finanças/cartao.js';
+import carregarDadosContasFixas from './planilhas/Finanças/contasFixas.js';
+
+carregarDadosAssinatura();
+carregarDadosContasFixas();
+carregarDadosCartao();
